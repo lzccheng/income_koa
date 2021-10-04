d35ca5ed5d4e88e46e2a9fc28b0d913856fad2d7
rocky <rocky@sdytw.onmicrosoft.comrocky@sdytw.onmicrosoft.com>
2021-10-04 23:12:13 +0800

    init

---
+++ src/nodeServer/server.js
@@ -0,0 +1,11 @@
+const { createServer } = require('http')
+
+const port = 80
+
+const app = createServer((req, res) => {
+    const { path } = req
+    console.log('path ===', req.headers)
+    res.end('node demo =====', path)
+})
+
+app.listen(port, () => console.log('running on http://localhost:' + port))
